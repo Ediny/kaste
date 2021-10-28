095637501ff09eff056b371fc5563c1acd6e34a7
Ediny <36110349+Ediny@users.noreply.github.com>
2021-10-28 20:50:32 +0300

    Add files via upload

---
+++ 2810_web/js/app.js
@@ -0,0 +1,9 @@
+const box = document.getElementsByClassName("box");
+const lid = document.getElementsByClassName("lid");
+
+
+
+function openBox() {
+    lid.style.transform = 'rotate(180deg)';
+}
+
